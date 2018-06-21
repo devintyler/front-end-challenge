c8f1f5fd6ab37bbe561a08bd02a0a90f113153bc
Devin Visslailli <devin.viss@gmail.com>
2018-06-21 14:13:47 -0400

    Add org model

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,9 @@
 // Use this file to create Immutable data models (if necessary)
-// import {
-// 	Record,
-// } from 'immutable';
+import {Record} from 'immutable';
 
-// export const Person = new Record({
-// 	firstName: 'Cowboy',
-// 	lastName: 'McClure',
-// 	isCool: false,
-// });
+export const Org = new Record({
+  orgName: '',
+  orgRepos: [],
+  loading: false,
+  error: null,
+});
