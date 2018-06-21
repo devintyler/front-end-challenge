7bdf9689571ff1f548039ed346f86ef22a678fa1
Devin Visslailli <devin.viss@gmail.com>
2018-06-21 14:32:33 -0400

    add reducers for generating repo states

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,43 @@
 import { combineReducers } from 'redux';
-// import * as types from '../constants';
-// import {
-// 	Person,
-// } from '../models';
+import * as types from '../constants';
+import {
+	Org,
+} from '../models';
 
-// const initialUserState = new Person();
+const initialState = new Org();
 
-const emptyReducer = (state = '', {
+const org = (state = initialState, {
 	type,
-	/* value, */
+	payload
 }) => {
 	switch (type) {
-	// case types.CHANGE_FIELD:
-	// 	return value;
-	default:
-		return state;
+		case types.SET_ORG:
+			return Object.assign({}, state, {
+				orgName: payload
+			});
+		case types.FETCH_DATA_BEGIN:
+			return Object.assign({}, state, {
+				loading: true,
+				error: null,
+			});
+		case types.FETCH_DATA_SUCCESS:
+			return Object.assign({}, state, {
+				loading: false,
+				orgRepos: payload,
+			});
+		case types.FETCH_DATA_FAIL:
+			return Object.assign({}, state, {
+				loading: false,
+				error: payload,
+				orgRepos: [],
+			});
+		default:
+			return state;
 	}
-};
+}
 
 const rootReducer = combineReducers({
-	emptyReducer,
+	org,
 });
 
 export default rootReducer;
