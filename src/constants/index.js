28cb926cb53b0f49eff028eae2f67afd54497375
Devin Visslailli <devin.viss@gmail.com>
2018-06-21 14:32:18 -0400

    Add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
 // Export constants here
-// export const COOL_CONSTANT = 'COOL_CONSTANT';
+export const SET_ORG = 'SET_ORG';
+export const FETCH_DATA_BEGIN = 'FETCH_DATA_BEGIN';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_DATA_FAIL = 'FETCH_DATA_FAIL';
